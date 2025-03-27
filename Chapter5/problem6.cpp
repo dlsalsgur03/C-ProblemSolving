b1eb5d2898fdf1fc717d6ae026d6fbede4ec25d8
dlsalsgur03 <dlsalsgur2@gmail.com>
2025-03-27 19:06:55 +0900

    실습문제 6번 문제풀이

---
+++ Chapter5/problem6.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char& find(char a[], char c, bool& success) {
+	for (int i = 0; i < strlen(a); i++) {
+		if (a[i] == c) {
+			success = true;
+			return a[i];
+		}
+	}
+}
+
+int main() {
+	char s[] = "Mike";
+	bool b = false;
+	char& loc = find(s, 'M', b);
+	if (!b) {
+		cout << "M을 발견할 수 없다." << '\n';
+		return 0;
+	}
+	loc = 'm';
+	cout << s << '\n';
+
+	return 0;
+}
