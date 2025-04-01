fe0098b57550d2ff24f90f9bdaae572039268f19
dlsalsgur03 <dlsalsgur2@gmail.com>
2025-04-01 16:10:17 +0900

    실습문제 1번 수정

--- Chapter5/problem1.cpp
+++ Chapter5/problem1.cpp
@@ -7,22 +7,20 @@ public:
 	Circle() { radius = 1; }
 	Circle(int radius) { this->radius = radius; }
 	int getRadius() { return radius; }
-	double getArea() { return 3.14 * radius * radius; }
-	void setRadius(int radius) { this->radius = radius; }
 };
 
 void swap(Circle& c1, Circle& c2) {
 	Circle tmp;
-	tmp.setRadius(c1.getRadius());
-	c1.setRadius(c2.getRadius());
-	c2.setRadius(tmp.getRadius());
+	tmp = c1;
+	c1 = c2;
+	c2 = tmp;
 }
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+
 	Circle donut(10);
 	Circle pizza(30);
 	cout << donut.getRadius() << '\t' << pizza.getRadius() << '\n';
