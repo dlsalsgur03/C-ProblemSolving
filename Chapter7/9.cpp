9c0d6431c7c428418e840a27c88f515a42e18c82
dlsalsgur03 <dlsalsgur2@gmail.com>
2025-04-12 15:06:32 +0900

    실습문제 9번 문제풀이

---
+++ Chapter7/9.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle(int radius = 0) { this->radius = radius; }
+	void show() { cout << "radius = " << radius << " ÀÎ ¿ø" << '\n'; }
+	friend Circle operator+ (int n, Circle op);
+};
+
+Circle operator+ (int n, Circle op) {
+	Circle tmp;
+	tmp = n + op.radius;
+	return tmp;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	Circle a(5), b(4);
+	b = 1 + a;
+	a.show();
+	b.show();
+}
